ba9ff53a14b288d2901921dd38f73fbba39708ba
Addy897 <adityasrivastava9943@gmail.com>
2026-04-13 23:58:14 +0530

    separte arg based calc

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 NAME="calc"
 nasm -f elf -o "${NAME}.o" "${NAME}.s" &&  ld -m elf_i386 "${NAME}.o" -o $NAME 
+nasm -f elf -o "${NAME}_cmd.o" "${NAME}_cmd.s" &&  ld -m elf_i386 "${NAME}_cmd.o" -o main
---
+++ calc_cmd.s
@@ -0,0 +1,233 @@
+
+section .data
+	err db "Error: Not int",0
+	len equ $ - err
+        usage_str db "Usage: calc [NUM1] [NUM2] [OP NUM]{1-ADD 2-SUB 3-MUL 4-DIV}",0xa
+	usage_str_len equ $ - usage_str
+        ans_txt db "Ans: ",0
+	ans_txt_len equ $ - ans_txt
+	err2 db "Invalid choice",0xa
+	err2_len equ $ - err2
+	err3 db "Divide by zero",0xa
+	err3_len equ $ - err3
+	num1 dd 0
+	num2 dd 0
+	choice db 0
+	ans dd 0
+	num_len equ 32
+section .bss
+	num resb num_len
+section .text
+global _start
+
+_start:
+
+        pop eax
+	cmp eax, 4
+        jne usage 
+	
+
+        ; input num 1
+        pop eax
+        call toint
+	mov [num1],eax
+	pop eax
+        ; input num 2
+        call toint
+	mov [num2],eax
+        pop eax
+
+
+	;operations	
+        call toint
+        cmp eax,1
+	je .add
+	cmp eax,2
+	je .sub
+	cmp eax,3
+	je .mul
+	cmp eax,4
+	je .div
+	push err2_len
+	push err2
+	call print
+	jmp exit
+	.add:
+		mov eax,[num1]
+		add eax,[num2]
+		jmp .done
+	.sub:
+		mov eax,[num1]
+		sub eax,[num2]
+		jmp .done
+	.mul:
+		mov eax,[num1]
+		mov ebx, [num2]
+		imul ebx
+		jmp .done
+	.div:
+		mov eax,[num1]
+		mov ebx,[num2]
+		cmp ebx,0
+		je .err
+		
+		cmp eax,0
+		jl .b1
+		jmp .b2
+		.b1:
+			mov edx,-1
+			imul edx
+			push eax
+			mov eax,ebx
+			mov edx,-1
+			imul edx
+			mov ebx,eax
+			pop eax
+			
+					
+		.b2:
+			xor edx,edx	
+			idiv ebx
+		jmp .done
+	.err:
+		push err3_len	
+		push err3
+		call print
+		jmp exit
+
+	.done:
+		mov [ans],eax
+		push ans_txt_len
+		push ans_txt
+		call print
+		push dword [ans]
+		mov dword [num], 0
+		call tostr
+		push eax
+		call print
+	jmp exit
+exit:
+		mov eax,1
+		mov ebx,1
+		int 0x80
+usage:
+        push usage_str_len
+	push usage_str
+	call print
+        jmp exit
+
+tostr:
+	push ebp
+	mov ebp ,esp
+	mov eax,[ebp+0x8]
+	lea edi, [num + 15]
+    	mov byte [edi], 0x0A
+	cmp eax,0
+	jl .negative
+	push 1
+	jmp .loop
+	.negative:
+		mov edx,-1
+		imul edx
+		push -1
+
+	.loop:
+		dec edi
+		xor edx, edx
+    		mov ecx, 10
+    		div ecx
+    		add dl, '0'
+    		mov [edi], dl
+    		test eax, eax
+    		jnz .loop	
+		
+	.done:
+		pop edx
+		cmp edx,-1
+		je .l3
+		jmp .l4
+		.l3:	
+			dec edi
+			mov byte [edi] ,'-'
+		.l4:
+			mov eax,edi
+			mov esp,ebp
+			pop ebp
+			ret 
+	
+toint:
+	push ebp
+	mov ebp,esp
+	mov ebx,[ebp+0x8]			
+	mov edx,0
+	cmp byte [ebx],'-'
+	jne .branch
+	mov ecx,-1
+	inc bl
+	jmp .loop2
+	.branch:
+		mov ecx,1
+	.loop2:
+		cmp byte [ebx],0xa
+		je .done
+		cmp byte [ebx],0
+		je .done
+
+                mov eax,[ebx]
+		sub al,'0'
+		cmp al,0
+		jl .error	
+		cmp al,9
+		jg .error
+		imul edx,10
+		add dl,al
+		inc bl
+		jmp .loop2
+	.error:
+		push len
+		push err
+		call print
+		call printn
+		mov esp ,ebp
+		pop ebp
+		jmp exit		
+	
+	.done:
+		imul edx,ecx
+		mov eax,edx
+		mov esp,ebp
+		pop ebp
+		ret	
+	
+
+input: 
+	push ebp
+	mov ebp,esp
+	mov ecx,[ebp+0x8]
+	mov edx,[ebp+0xc]
+	mov eax,3
+	mov ebx,0
+	int 0x80
+	pop ebp
+	ret
+
+
+print:
+	push ebp
+	mov ebp,esp
+	mov ecx,[ebp+0x8]
+	mov edx,[ebp+12]
+	mov eax,4
+	mov ebx,1
+	int 0x80
+	pop ebp
+	ret
+printn:
+	push 0xa
+	mov eax, 4
+	mov ebx, 1
+	mov edx, 1
+	mov ecx,esp
+	int 0x80
+	pop ecx
+	ret
